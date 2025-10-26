a5ebc735cbb1d433efd05af9d43abd0e685e607a
raghulmanik <raghulmanik.dev@gmail.com>
2025-10-26 16:45:53 +0530

    add playground/loop/fib.js and test fib using loop

---
+++ playground/loop/fib.js
@@ -0,0 +1,41 @@
+const log = console.log;
+
+function fibForLoop(n){
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+
+    let a = 0;
+    let b= 1 ;
+    let ans;
+
+    for (let i = 2; i <= n ; i++) {
+       ans = a + b
+       log(` a -> ${a} and b -> ${b} before`)
+       a = b;
+       b = ans;
+       log(` a -> ${a} and b -> ${b} after`)
+       log(`${ans} in ${i}th iteration.`);
+    }
+    return ans ;
+}
+
+function fibWhileLoop(n){
+
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+
+    let ans;
+
+    // we need to start at 2nd iteration because 0 and 1 is done for you.
+    let i = 2;
+    let a = 0;
+    let b = 1;
+
+    while(i <= n){
+       ans = a + b;
+       a = b;
+       b = ans;
+       i++;
+    }
+    return ans;
+}
